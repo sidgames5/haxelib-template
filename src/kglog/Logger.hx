5919bf0d6f31e071b6489b7739554bcdfadd3507
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-15 09:11:51 -0500

    Create Logger.hx

---
+++ src/kglog/Logger.hx
@@ -0,0 +1,17 @@
+package kglog;
+
+class Logger {
+  /**
+   * This function literally does nothing
+   */
+  public function new():Void {}
+  
+  /**
+   * Send a string of text to the program's log file
+   * and to the console.
+	 * @param  Text  String of text to log.
+   */
+  public static function simpleLog(Text:String) {
+    Console.log(Text);
+  }
+}
